c5a426949261bb743a4e643317ad436b928e3af0
SarveshSrivastava <ssarveshss2997@gmail.com>
2024-01-22 16:41:44 +0530

    Lgin-Logout, search functionality added

--- Components/Header.js
+++ Components/Header.js
@@ -1,18 +1,26 @@
-
-import "../App.css"
+import { useState } from "react";
+import "../App.css";
 import logo from "../assets/logo.png";
 const Header = () => {
+  const [btn, setBtn] = useState("Login");
+  const handleClick = () => {
+    btn === "Login" ? setBtn("Logout") : setBtn("Login");
+  };
+
   return (
     <div className="HeaderComponent">
       <>
-      <img src={logo} alt="food-ordering-application-logo"/>
+        <img src={logo} alt="food-ordering-application-logo" />
       </>
       <>
-      <ul className="nav-items">
-        <li>Home</li>
-        <li>About Us</li>
-        <li>Cart</li>
-      </ul>
+        <ul className="nav-items">
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Cart</li>
+          <button className="btn" onClick={handleClick}>
+            {btn}
+          </button>
+        </ul>
       </>
     </div>
   );
--- Components/Restaurants.js
+++ Components/Restaurants.js
@@ -4,35 +4,61 @@ import { restaurantsList } from "../mock/restaurants";
 import Shimmer from "./Shimmer";
 const Restaurants = () => {
   const [restaurants, setRestaurants] = useState([]);
+  const [filteredRestaurants, setFilteredRestaurants] = useState([]);
+  const [searchText,setSearchText] = useState("");
+
+  const handleChange=(e)=>{
+    setSearchText(e.target.value)
+  }
+
+  const handleSearchClick=()=>{
+    const fR = restaurants.filter(x=>x.info.name.toLowerCase().includes(searchText.toLowerCase()))
+    setFilteredRestaurants(fR);
+  }
 
   const handleClick = () => {
-    filteredRestaurants = restaurants.filter((x) => x.info.avgRating > 4.4);
-    console.log(filteredRestaurants);
-    setRestaurants(filteredRestaurants);
+    const fR = restaurants.filter((x) => x.info.avgRating > 4.1);
+    console.log(fR);
+    setFilteredRestaurants(fR);
   };
 
   useEffect(() => {
     fetchSwiggyData();
   }, []);
 
-  const fetchSwiggyData=async ()=>{
-    const swiggyData=await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.34991224864122&lng=78.41912437230349&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
-    const swiggyJson=await swiggyData.json();
-    console.log(swiggyJson?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
-    setRestaurants(swiggyJson?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
-  }
+  const fetchSwiggyData = async () => {
+    const swiggyData = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.34991224864122&lng=78.41912437230349&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const swiggyJson = await swiggyData.json();
+    console.log(
+      swiggyJson?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants
+    );
+    setRestaurants(
+      swiggyJson?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants
+    );
+    setFilteredRestaurants(swiggyJson?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
+      ?.restaurants);
+  };
 
-  if(restaurants.length===0)
-    return <Shimmer/>
-  return (
+  // if(restaurants.length===0)
+  //   return <Shimmer/>
+
+  return restaurants.length === 0 ? (
+    <Shimmer />
+  ) : (
     <>
       <div>
+        <input type="text" value={searchText} onChange={handleChange}/>
+        <button className="searchButton" onClick={handleSearchClick}>SEARCH</button>
         <button id="filterbutton" onClick={handleClick}>
           Filter top rated restaurants
         </button>
       </div>
       <div className="RestaurantsComponents">
-        {restaurants.map((x) => (
+        {filteredRestaurants.map((x) => (
           <RestaurantCard
             key={x.info.id}
             name={x.info.name}
